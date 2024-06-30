8b049f37ee44e8ab0ac20866247348ebfd6ac800
Bartek Bielawski <bartek.bielawski@live.com>
2024-06-30 17:28:15 +0200

    Some more demo slides

---
+++ Demos/SampleFiles/FibreChannelSwitch.psm1
@@ -0,0 +1,31 @@
+﻿function Set-FCPort {
+    param (
+        $Switch,
+        $Port,
+        $ConnectedDevice,
+        $ConnectedPort,
+        [ValidateSet(
+                'Enabled',
+                'Disabled'
+        )]
+        $State
+    )
+
+    switch ($State) {
+        Enabled {
+            [PSCustomObject]@{
+                Switch = $Switch
+                Port = $Port
+                Description = '{0}_{1}' -f $ConnectedDevice, $ConnectedPort
+            }
+        }
+        Disabled {
+            [PSCustomObject]@{
+                Switch = $Switch
+                Port = $Port
+                Description = 'port{0}' -f $Port
+            }
+        }
+    }
+}
+
---
+++ Presentation/0.ps1
@@ -0,0 +1,25 @@
+﻿$creds = Import-Clixml -Path C:\RecycleBin\creds.clixml
+$session = New-PSSession -ComputerName 192.168.56.10 -Credential $creds
+$sampleFolder = Join-Path $PSScriptRoot -ChildPath SampleFiles
+if ($env:PSModulePath -split ';' -notcontains $sampleFolder) {
+    $env:PSModulePath += ";$sampleFolder"
+}
+
+$3 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '3.xaml' })[0]
+$2 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '2.xaml' })[0]
+$1 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '1.xaml' })[0]
+$0 = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '0.ps1' })[0]
+$main = @($psISE.CurrentPowerShellTab.Files.Where{ $_.DisplayName -eq '00_Title.xaml' })[0]
+
+Start-Sleep -Milliseconds 100
+$wshell.SendKeys('^r')
+do {
+    Start-Sleep -Seconds 10
+} while ((Get-Date).Hour -lt 9)
+
+# 3... 2... 1... countdown - PS ISE style. :P
+foreach ($file in $0, $3, $2, $1) {
+    Start-Sleep -Seconds 1
+    $null = $psISE.CurrentPowerShellTab.Files.Remove($file)
+}
+$psISE.CurrentPowerShellTab.Files.SetSelectedFile($main)
